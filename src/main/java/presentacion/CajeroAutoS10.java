b211e650a5f728b61a5e39755b202fec2834e8cf
PieroL1 <202114037@uns.edu.pe>
2025-02-28 08:43:51 -0500

    Commit 1

---
+++ src/main/java/presentacion/CajeroAutoS10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package presentacion;
+
+/**
+ *
+ * @author Chrome
+ */
+public class CajeroAutoS10 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
